3791a71a4f68a48351f2a05cdd23d57c1cd618e6
janecyyu <janecyyu@gmail.com>
2020-06-12 08:44:04 -0700

    add append

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,21 @@
+from collections import deque
+
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.remaining = capacity
 
     def append(self, item):
-        pass
+        self.remaining -= 1
+        q = deque()
+
+        if self.remaining <= self.capacity and self.remaining >= 0:
+            q.append(item)
+        elif self.remaining < 0:
+            q.popleft()
+            q.append(item)
+        return q
 
     def get(self):
         pass
